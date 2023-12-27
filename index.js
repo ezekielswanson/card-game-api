2ecd3c6a042b29526a15f0ee5a21db0200c19788
ezekielswanson <swansonezekiel@gmail.com>
2023-12-26 20:11:49 -0600

    Updated js

--- index.js
+++ index.js
@@ -9,14 +9,18 @@ const remainingText = document.getElementById("remaining")
 const computerScoreEl = document.getElementById("computer-score")
 const myScoreEl = document.getElementById("my-score")
 
+
+//change api url
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
             remainingText.textContent = `Remaining cards: ${data.remaining}`
             deckId = data.deck_id
-            console.log(deckId)
+            console.log(click)
         })
+
+
 }
 
 newDeckBtn.addEventListener("click", handleClick)
